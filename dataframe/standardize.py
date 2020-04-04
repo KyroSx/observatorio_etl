44ea0154014619a98c265887f326e6b6612ac1d9
KyroSx <leonardo@localhost.localdomain>
2020-04-04 10:32:09 -0300

    feat: data validation for column  2007

--- dataframe/standardize.py
+++ dataframe/standardize.py
@@ -1,4 +1,5 @@
 import re
+import locale
 
 def t_parenthesis(cell):
     return cell.replace('(', '').replace(')', '')
@@ -6,24 +7,21 @@ def t_parenthesis(cell):
 def t_comma(cell):
     return cell.replace('.', '').replace(',', '.')
 
-def standardize(cell):    
+def standardize(cell): 
+    locale.setlocale(locale.LC_NUMERIC, "en_DK.UTF-8") 
     value = ''
     regexs = {
         'correct': re.compile(r'^\d*\.?\d*$'),
         'hyphen': re.compile(r'^\-$'),
-        'comma': re.compile(r'^\d*\.?\d*\,?\d*$'),
-        'parenthesis': re.compile(r'^\(\d*\.?\d*\,?\d*\)$')
     }
     
     if regexs['correct'].match(cell):
         value = cell
     elif regexs['hyphen'].match(cell):
         value = '0'
-    elif regexs['comma'].match(cell):
-        value = t_comma(cell)
-    elif regexs['parenthesis'].match(cell):
+    else:
         value = t_parenthesis(cell)
-        value = t_comma(value)
+        value = locale.atof(value)
     
     return float(value)
     
