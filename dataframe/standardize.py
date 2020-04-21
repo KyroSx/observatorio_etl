a54be4d874fec9ea59421f91559eea2dfffee468
KyroSx <leonardo@localhost.localdomain>
2020-04-21 11:05:32 -0300

    nothing important change

--- dataframe/standardize.py
+++ dataframe/standardize.py
@@ -7,8 +7,8 @@ def t_parenthesis(cell):
 def t_comma(cell):
     return cell.replace('.', '').replace(',', '.')
 
-def standardize(cell): 
-    locale.setlocale(locale.LC_NUMERIC, 'en_DK.UTF-8') 
+def standardize(cell):
+    locale.setlocale(locale.LC_NUMERIC, 'en_DK.UTF-8')
     value = ''
     regexs = {
         'correct': re.compile(r'^\d*\.?\d*$'),
@@ -16,10 +16,10 @@ def standardize(cell):
     }
 
     cell = str(cell)
-    
+
     if type(cell) is str:
         cell = cell.strip()
-    
+
     if regexs['correct'].match(cell):
         value = cell
     elif regexs['hyphen'].match(cell):
@@ -27,7 +27,5 @@ def standardize(cell):
     else:
         value = t_parenthesis(cell)
         value = locale.atof(value)
-    
-    return float(value)
-    
 
+    return float(value)
