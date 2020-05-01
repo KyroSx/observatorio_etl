f342766b01c2e75ab7c70b59fcf5416d23d434c1
KyroSx <leonardo@localhost.localdomain>
2020-04-30 21:26:02 -0300

    refact: using Black to styled guide the code

--- dataframe/dataframe.py
+++ dataframe/dataframe.py
@@ -1,24 +1,26 @@
 import pandas
 import numpy as np
 
+
 def usecols():
-    cols = [i for i in range(1,18)]
+    cols = [i for i in range(1, 18)]
     del cols[2]
     return cols
 
+
 def get_dataframe():
-    file_path = './data/FUNDEB_Municipio.csv'
-    encode = 'windows-1252'
+    file_path = "./data/FUNDEB_Municipio.csv"
+    encode = "windows-1252"
     cols = usecols()
 
     fundeb = pandas.read_csv(
         filepath_or_buffer=file_path,
         encoding=encode,
-        sep=';',
-        decimal=',',
-        engine='c',
+        sep=";",
+        decimal=",",
+        engine="c",
         usecols=cols,
-        nrows=120
+        nrows=24,
     )
 
     return fundeb
--- dataframe/standardize.py
+++ dataframe/standardize.py
@@ -1,18 +1,21 @@
 import re
 import locale
 
+
 def t_parenthesis(cell):
-    return cell.replace('(', '').replace(')', '')
+    return cell.replace("(", "").replace(")", "")
+
 
 def t_comma(cell):
-    return cell.replace('.', '').replace(',', '.')
+    return cell.replace(".", "").replace(",", ".")
+
 
 def standardize(cell):
-    locale.setlocale(locale.LC_NUMERIC, 'en_DK.UTF-8')
-    value = ''
+    locale.setlocale(locale.LC_NUMERIC, "en_DK.UTF-8")
+    value = ""
     regexs = {
-        'correct': re.compile(r'^\d*\.?\d*$'),
-        'hyphen': re.compile(r'^\-$'),
+        "correct": re.compile(r"^\d*\.?\d*$"),
+        "hyphen": re.compile(r"^\-$"),
     }
 
     cell = str(cell)
@@ -20,10 +23,10 @@ def standardize(cell):
     if type(cell) is str:
         cell = cell.strip()
 
-    if regexs['correct'].match(cell):
+    if regexs["correct"].match(cell):
         value = cell
-    elif regexs['hyphen'].match(cell):
-        value = '0'
+    elif regexs["hyphen"].match(cell):
+        value = "0"
     else:
         value = t_parenthesis(cell)
         value = locale.atof(value)
