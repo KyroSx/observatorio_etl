feae9d9c633741833e935a10b04267fc4706b8b8
leonardo <leolub150@gmail.com>
2020-05-14 18:28:12 -0300

    fix: return not working, back to print

--- periods/period.py
+++ periods/period.py
@@ -9,6 +9,7 @@ def period_options(period) -> int:
 
     return periods.get(period, "Not a valid period")
 
+
 def get_period(period, city_data, city_name, year):
     period_value = period_options(period)
 
@@ -18,10 +19,13 @@ def get_period(period, city_data, city_name, year):
     city_data_year = city_data[year]
 
     size = int(len(city_data_year)/period_value)
+    print(f'size -> {size}')
     for i in range(size):
         city_period_sliced = city_data_year[start_interval:end_interval]
         city_period_sum = city_period_sliced.sum()
 
+        print(f"{city_name}#{year}:")
+        print(f"sum is: {city_period_sum}")
         start_interval = end_interval
         end_interval += period_value
 
