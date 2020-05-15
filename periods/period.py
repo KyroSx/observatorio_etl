9e5ab6673ea3e3616f1a859841c1fcaf3487c6bb
leonardo <leolub150@gmail.com>
2020-05-15 18:33:49 -0300

    feat: the function was refactored. Now it returns a list with multiples Data Object.

--- periods/period.py
+++ periods/period.py
@@ -1,3 +1,5 @@
+from data.Data_Object import create_data_object
+
 
 def period_options(period) -> int:
     periods = {
@@ -6,27 +8,43 @@ def period_options(period) -> int:
         "semestral": 6,
         "anual": 12
     }
-
     return periods.get(period, "Not a valid period")
 
 
-def get_period(period, city_data, city_name, year):
-    period_value = period_options(period)
+def get_sums_list(period_value, city_data_year) -> list:
+    city_period_sum_list = []
 
     start_interval = 0
     end_interval = period_value
 
-    city_data_year = city_data[year]
-
     size = int(len(city_data_year)/period_value)
-    print(f'size -> {size}')
+
     for i in range(size):
         city_period_sliced = city_data_year[start_interval:end_interval]
         city_period_sum = city_period_sliced.sum()
 
-        print(f"{city_name}#{year}:")
-        print(f"sum is: {city_period_sum}")
+        city_period_sum_list.append(city_period_sum)
+
         start_interval = end_interval
         end_interval += period_value
 
-    return city_period_sum
+    return city_period_sum_list
+
+
+def get_period_object_list(period, city_data, city_name, year) -> list:
+    period_value = period_options(period)
+
+    city_data_year = city_data[year]
+
+    city_period_sum_list = get_sums_list(period_value, city_data_year)
+    city_period_object_list = []
+
+    for index, value in enumerate(city_period_sum_list):
+        data_o = create_data_object(
+            period, value,
+            year, city_name,
+            index + 1
+        )
+        city_period_object_list.append(data_o)
+
+    return city_period_object_list
