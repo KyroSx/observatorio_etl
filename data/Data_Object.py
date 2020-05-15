feb251402d16a50eb6d4d5021abd6570d4a6ef6b
leonardo <leolub150@gmail.com>
2020-05-15 18:40:19 -0300

    feat: new class created, it stores all the values extracted in other operations. Will be inserted in the database in the future

---
+++ data/Data_Object.py
@@ -0,0 +1,24 @@
+
+class Data_Object:
+    def __init__(self,
+                 period_type, value,
+                 location_year, location_city_name,
+                 index):
+        self.period_type = period_type
+        self.value = value
+        self.location_year = location_year
+        self.location_city_name = location_city_name
+        self.index = index
+
+    def __str__(self):
+        a = f"{self.location_city_name}#{self.location_year}:\n"
+        b = f"{self.index}º {self.period_type} -> ${self.value}"
+        return a + b
+
+
+def create_data_object(period, value,
+                       year, city_name,
+                       index):
+    data_o = Data_Object(period, value,
+                         year, city_name, index)
+    return data_o
