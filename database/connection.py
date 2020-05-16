5b2883a3656f6d69cf6f7983c7bf7e96be73fbd0
leonardo <leolub150@gmail.com>
2020-05-16 17:07:14 -0300

    feat: connection function to OO
    transforming a function into a class, with more connection methods.

--- database/connection.py
+++ database/connection.py
@@ -1,28 +1,39 @@
 import mysql.connector
-import database_env as db
 
+from . import database_env as db
 from mysql.connector import errorcode
 
 
-config = {
-    'user': db.DB_USER,
-    'password': db.DB_PASSWORD,
-    'host': db.HOST,
-    'database': db.DB_NAME,
-    'raise_on_warnings': True
-}
+class Database:
+    cnx = ''
+    config = {
+        'user': db.DB_USER,
+        'password': db.DB_PASSWORD,
+        'host': db.HOST,
+        'database': db.DB_NAME,
+        'raise_on_warnings': True
+    }
 
+    def start_connection(self):
+        try:
+            self.cnx = mysql.connector.connect(**self.config)
+        except mysql.connector.Error as err:
+            if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+                print('Something is wrong with your user or password')
+            elif err.errno == errorcode.ER_BAD_DB_ERROR:
+                print('Database does not exist')
+            else:
+                print(err)
 
-def connect_to_db():
-    try:
-        cnx = mysql.connector.connect(**config)
-    except mysql.connector.Error as err:
-        if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
-            print('Something is wrong with your user or password')
-        elif err.errno == errorcode.ER_BAD_DB_ERROR:
-            print('Database does not exist')
-        else:
-            print(err)
-        cnx.close()
-    else:
-        return cnx
+    def close_connection(self):
+        self.cnx.close()
+
+    def search_city_name_query(self, city_name):
+        if self.cnx is not None:
+            cursor = self.cnx.cursor()
+            query = f'SELECT idIBGE FROM locations '
+            query += f'WHERE name = \'{city_name}\''
+
+            cursor.execute(query)
+            city_id = [str(x) for x in cursor]
+            print(city_id[0])
