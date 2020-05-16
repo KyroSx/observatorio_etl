75311ff58e150ed33b82dc3485fc50644cf12e46
leonardo <leolub150@gmail.com>
2020-05-16 17:08:49 -0300

    no relevant changes

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas
-from database.connection import connect_to_db
+
+from database.connection import Database
 from dataframe.dataframe import get_dataframe
 from dataframe.standardize import standardize
 from periods.period import get_period_object_list
@@ -21,6 +22,13 @@ for city_name, city_df in citys:
         list_data_objects += get_period_object_list(
             period_type, city_df, city_name, year)
 
+a = []
+[a.append(x)
+ for x in list_data_objects[::-1] if x.location_city_name == 'Assis Brasil']
 
-[print(f'{x}\n{space}')
- for x in list_data_objects[::-1]]
+db = Database()
+db.start_connection()
+
+db.search_city_name_query('Acrêlandia')
+
+db.close_connection()
