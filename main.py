f053d033a13db3847710508c5a990ce73af2b78d
leonardo <leolub150@gmail.com>
2020-05-19 18:55:23 -0300

    no relevant changes

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pandas
 
 from database.connection import Database
-from dataframe.dataframe import get_dataframe
 from dataframe.standardize import standardize
 from periods.period import get_period_object_list
 
-fundeb = get_dataframe()
+from data.Data_Object import create_data_object
+from dataframe.dataframe import create_data_frame
+
+
+fundeb = create_data_frame(number_of_rows=12)
 
 years = [f"{i}" for i in range(2007, 2020)]
 space = "-" * 33
 list_data_objects = []
+
 
 for year in years:
     fundeb[year] = fundeb[year].apply(standardize)
@@ -17,18 +21,11 @@ for year in years:
 citys = fundeb.groupby("Município")
 for city_name, city_df in citys:
     print(f'Loading for {city_name}...')
+    period_type = "anual".lower()
     for year in years:
         period_type = "anual".lower()
         list_data_objects += get_period_object_list(
             period_type, city_df, city_name, year)
 
-a = []
-[a.append(x)
- for x in list_data_objects[::-1] if x.location_city_name == 'Assis Brasil']
-
-db = Database()
-db.start_connection()
-
-db.search_city_name_query('Acrêlandia')
-
-db.close_connection()
+[print(f'{x}\n{space}')
+ for x in list_data_objects[::-1]]
