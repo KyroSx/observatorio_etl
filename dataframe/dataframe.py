33a1d82df0cf04bdfe37b7bcc47dc92b8a4b334b
leonardo <leolub150@gmail.com>
2020-05-19 18:01:02 -0300

    refact: using factory pattern

--- dataframe/dataframe.py
+++ dataframe/dataframe.py
@@ -1,26 +1,32 @@
 import pandas
-import numpy as np
 
 
-def usecols():
-    cols = [i for i in range(1, 18)]
-    del cols[2]
-    return cols
+class DataFrame:
+
+    def usecols(self):
+        cols = [i for i in range(1, 18)]
+        del cols[2]
+        return cols
+
+    def get_dataframe(self, number_of_rows):
+        file_path = "./data/FUNDEB_Municipio.csv"
+        encode = "windows-1252"
+        cols = self.usecols()
+
+        fundeb = pandas.read_csv(
+            filepath_or_buffer=file_path,
+            encoding=encode,
+            sep=";",
+            decimal=",",
+            engine="c",
+            usecols=cols,
+            nrows=number_of_rows,
+        )
+
+        return fundeb
 
 
-def get_dataframe():
-    file_path = "./data/FUNDEB_Municipio.csv"
-    encode = "windows-1252"
-    cols = usecols()
-
-    fundeb = pandas.read_csv(
-        filepath_or_buffer=file_path,
-        encoding=encode,
-        sep=";",
-        decimal=",",
-        engine="c",
-        usecols=cols,
-        nrows=24,
-    )
-
-    return fundeb
+def create_data_frame(number_of_rows=66829):
+    df_oo = DataFrame()
+    if number_of_rows is not None:
+        return df_oo.get_dataframe(number_of_rows)
