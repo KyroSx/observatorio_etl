9253932deb8bac13c6b3697219ce555c57786224
leonardo <leolub150@gmail.com>
2020-05-29 09:41:09 -0300

    tests

--- tests/unit/DataBase_Operation_test.py
+++ tests/unit/DataBase_Operation_test.py
@@ -1,25 +1,68 @@
 import unittest
+from datetime import datetime
 from database.connection import create_database_object
 from helpers.str_helper import remove_parenthesis, remove_char_from_string
 from data.Data_Object import create_data_object, Data_Object
+from periods.reference_period import transform_string_to_date
 
 
 class TestDataBase(unittest.TestCase):
+
     def test_get_city_id_by_name(self):
+        ''' Test if the data:name == locations:id '''
+
         city_name = ['Acrelândia', 'Guarapuava', 'Prudentópolis']
         city_id_list = ['1200013', '4109401', '4120606']
-        city_id_returned_list = []
+
+        info = {
+            'input_list': city_name,
+            'correct_list': city_id_list,
+            'output_number': 0
+        }
+        database = create_database_object()
+        self.test_generic_query_db(function=database.get_city_id_by_name,
+                                   info=info)
+
+    def test_get_reference_period_id_from_period(self):
+        datetime_format = '%Y-%m-%d'
+        def parse(string): return datetime.strptime(
+            string, datetime_format).date()
+
+        in_until_list = [
+            {"in": parse('2007-01-01'), "until": parse('2007-02-28')}
+        ]
+
+        correct_reference_id = [None]
+
+        info = {
+            'input_list': in_until_list,
+            'correct_list': correct_reference_id,
+            'output_number': 0
+        }
 
         database = create_database_object()
-        for index in range(len(city_name)):
-            city_id_returned = database.get_city_id_by_name(city_name[index])
-            city_id_returned_list.append(city_id_returned[0])
 
-            city_id_returned_list = list(city_id_returned_list)
+        self.test_generic_query_db(
+            info=info,
+            function=database.get_reference_period_id_from_periods_dates)
 
-        self.assertListEqual(city_id_returned_list, city_id_list)
+    def test_generic_query_db(self, info={}, function=None):
+        """ Template test for database querys 🎲 """
+        if not info and not function:
+            return
+
+        input_list, correct_list, output_number = info.values()
+        output_list = []
+
+        for input_item in input_list:
+            output_item = function(input_item)
+            if output_item:
+                output_list.append(output_item[output_number])
+
+        self.assertListEqual(output_list, correct_list)
 
     def test_insert_data_object(self):
+        ''' Test if a data object is inserted on database '''
         data_dict = {
             "period": 'yearly',
             "value": 3773651.1,
