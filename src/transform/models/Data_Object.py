50e73f2e252157c5872bd0c1580b7cf3b95cc74b
leonardo <leolub150@gmail.com>
2020-05-31 15:41:54 -0300

    feat: list of objects factory

--- src/transform/models/Data_Object.py
+++ src/transform/models/Data_Object.py
@@ -3,17 +3,23 @@ from dataclasses import dataclass
 
 @dataclass
 class Data:
+    """ Representation of Data Table on DW """
     data: float
     idInformation: int
     idInformationDataType: int
-    idPeriod: int
+    idPeriode: int
     idLocation: int
     idGranularity: int
+
+    def __str__(self):
+        string = f'({self.data}, {self.idInformation}, {self.idPeriode}, '
+        string += f'{self.idLocation}, {self.idGranularity})'
+        return string
 
 
 @dataclass
 class Data_Object:
-    """ Representation of Data Table on DW """
+    """ Representation Periode Sum """
     granularity: str
     value: float
     location_year: str
@@ -21,14 +27,24 @@ class Data_Object:
     index: int
 
     def __str__(self):
-        string = f''
         string = f"{self.location_city_name}#{self.location_year}:\n"
         string += f"{self.index}º {self.granularity} -> ${self.value}"
+
         return string
 
 
-def create_data_object(data_dict):
-    period, value, year, city_name, index = data_dict.values()
-    data_o = Data_Object(period, value,
-                         year, city_name, index)
+def create_data_object(data_dict: {}):
+    city_name, year, index, period, value = data_dict.values()
+    data_o = Data_Object(granularity=period, value=value,
+                         location_year=year, location_city_name=city_name,
+                         index=index)
     return data_o
+
+
+def create_object_list(dict_list):
+    data_object_list = []
+    for data_dict in dict_list:
+        data = create_data_object(data_dict)
+        data_object_list.append(data)
+
+    return data_object_list
