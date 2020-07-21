adfd0e62d9a14e19ddd41d5fd4eb1324caf159b8
leonardo <leonardo@pop-os.localdomain>
2020-07-21 14:08:07 -0300

    add a class to represent Data in the dw

---
+++ models/DataDW.py
@@ -0,0 +1,11 @@
+from dataclasses import dataclass
+
+
+@dataclass
+class DataDW:
+    data: float
+    id_information: int
+    id_information_datatype: int
+    id_reference_period: int
+    id_location: int
+    id_granularity: int
